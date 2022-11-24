76a35eab4e3403d70b1a60f8d374a4fe9df0305b
Arthur P <arthur30700@gmail.com>
2022-11-24 02:35:15 +0100

    Added input select and guide + Debugging inputs

--- assets/js/ssbutrainer.js
+++ assets/js/ssbutrainer.js
@@ -4,32 +4,37 @@
 // -- -- -- -- -- -- -- CONSTANTS AND VARIABLES -- -- -- -- -- -- -- \\
 // -- -- -- -- -- -- --       -- -- -- --       -- -- -- -- -- -- -- \\
 // HTML elements
+let inputSelect = document.getElementsByClassName("select-input");
+let inputImage = document.getElementById("input-img");
+let inputText = document.getElementById("input-p");
 let gamepadInfo = document.getElementById("gamepad-info");
-let stick = document.getElementById("left-stick");
+let stickLeft = document.getElementById("left-stick");
 let leftVis = document.getElementById("left-vis");
 let leftDeadZone = document.getElementById("left-dz");
 let angleInfo = document.getElementById("angle-info");
+
 
 // Constants
 const frameLength = 1e3 / 60;
 const audioPlayer = new Audio();
 audioPlayer.volume = 0.05;
 const sounds = {
-    "sf_hadoken": "assets/sounds/sf_hadoken.mp3",
-    "sf_shoryuken": "assets/sounds/sf_shoryuken.mp3",
-    "ken_shoryuken": "assets/sounds/ken_shoryuken.wav"
+    "hado_l": "assets/sounds/sf_hadoken.mp3",
+    "hado_r": "assets/sounds/ryu_hadoken.wav",
+    "shoryu_l": "assets/sounds/sf_shoryuken.mp3",
+    "shoryu_r": "assets/sounds/ken_shoryuken.wav"
 };
 const inputsOrder = {
-    "hadoken_L": [7, 6, 5],
-    "hadoken_R": [7, 8, 1],
-    "shoryuken_L": [5, 7, 6],
-    "shoryuken_R": [8, 7, 8],
-    "shaku_L": [8, 7, 6, 5],
-    "shaku_R": [6, 7, 8, 1]
+    "hado_l": [7, 6, 5],
+    "hado_r": [7, 8, 1],
+    "shoryu_l": [5, 7, 6],
+    "shoryu_r": [8, 7, 8],
+    "shaku_l": [8, 7, 6, 5],
+    "shaku_r": [6, 7, 8, 1]
 };
 const numAnnotation = ["RIGHT", "UP-RIGHT", "UP", "UP-LEFT", "LEFT", "DOWN-LEFT", "DOWN", "DOWN-RIGHT"];
-const controllerSettings = { "A": 0, "B": 1, "L_stick": "0,1", "R_stick": "2,3", "L_trigger": 3, "R_trigger": 4, "Deadzone": 10 };
-const bufferSize = 50;
+const controllerSettings = { "A": 0, "B": 1, "L_stick": "0,1", "R_stick": "2,3", "L_trigger": 3, "R_trigger": 4, "Deadzone": 15 };
+const bufferSize = 10;
 const buffer = new Array(bufferSize);
 buffer.fill({ direction: -1, time: window.performance.now() });
 buffer.push = function () {
@@ -41,6 +46,8 @@ buffer.push = function () {
 // Variables
 let released = true;
 let used = false;
+let chosenInput = `${inputSelect[0].value}_${inputSelect[1].value}`;
+inputImage.src = `assets/img/${chosenInput}.png`;
 let rAF;
 let gui;
 
@@ -64,7 +71,12 @@ window.requestAnimationFrame = function (f) {
 window.addEventListener("gamepadconnected", () => {
     var gp = navigator.getGamepads()[0];
     gamepadInfo.style.display = "none";
-    stick.style.display = "block";
+    inputImage.style.display = "block";
+    inputText.style.display = "block";
+    Array.from(inputSelect).forEach((select) => {
+        select.style.display = "block";
+    });
+    stickLeft.style.display = "block";
     angleInfo.style.display = "block";
 
     gui = new dat.GUI({ name: "Controller settings" });
@@ -81,12 +93,24 @@ window.addEventListener("gamepadconnected", () => {
 
 // Gamepad disconnected event 
 window.addEventListener("gamepaddisconnected", () => {
-    stick.style.display = "none";
+    stickLeft.style.display = "none";
     angleInfo.style.display = "none";
     gamepadInfo.style.display = "block";
     gamepadInfo.innerHTML = "Waiting for gamepad";
+    inputImage.style.display = "none";
+    inputText.style.display = "none";
+    Array.from(inputSelect).forEach((select) => {
+        select.style.display = "none";
+    });
     gui.destroy();
     window.cancelAnimationFrame(rAF);
+});
+
+Array.from(inputSelect).forEach((element) => {
+    element.addEventListener("change", () => {
+        chosenInput = `${inputSelect[0].value}_${inputSelect[1].value}`;
+        inputImage.src = `assets/img/${chosenInput}.png`;
+    });
 });
 
 
@@ -100,20 +124,30 @@ const checkThreeMotionInput = (bufferCopy, inputsOrder) => {
 
     // sliced(indexSecond) changed the order of the buffer
     indexFirst += indexSecond;
+    console.log(`-------------------------------------`);
+    console.log(bufferCopy);
+    console.log(`indexFirst : ${indexFirst}`);
+    console.log(`indexSecond : ${indexSecond}`);
+    console.log(`indexThird : ${indexThird}`);
 
     if (indexThird == -1 || indexSecond == -1 || (indexFirst - indexSecond) == -1 || indexThird > indexSecond || indexSecond > indexFirst || indexThird > indexFirst) {
         console.log(`Failed : You must input ${numAnnotation[inputsOrder[0] - 1]} -> ${numAnnotation[inputsOrder[1] - 1]} -> ${numAnnotation[inputsOrder[2] - 1]}`);
         return false;
     }
 
-    if ((bufferCopy[indexSecond].time - bufferCopy[indexFirst].time) > (10 * frameLength)) {
+    if ((bufferCopy[indexThird].time - bufferCopy[indexSecond].time) > (10 * frameLength)) {
         console.log(`Failed : Too much time on ${numAnnotation[inputsOrder[1] - 1]}`);
+        console.log(bufferCopy[indexThird].time - bufferCopy[indexSecond].time);
+        console.log((10 * frameLength));
         return false;
-    } else if ((bufferCopy[0].time - bufferCopy[indexSecond].time) > (11 * frameLength)) {
-        console.log("Failed : Pressed A too late");
+    } else if ((bufferCopy[0].time - bufferCopy[indexSecond].time) > (12 * frameLength)) {
+        console.log("Failed : Pressed attack button too late");
+        console.log(bufferCopy[0].time - bufferCopy[indexSecond].time);
+        console.log((12 * frameLength));
         return false;
     }
 
+    console.log(`-------------------------------------`);
     // Add how much time A pressed
 
     return true;
@@ -139,30 +173,34 @@ const update = (timeStamp) => {
     let leftDistance = (Math.sqrt((gp.axes[controllerSettings.L_stick[0]] ** 2 + gp.axes[controllerSettings.L_stick[2]] ** 2)) * 100).toFixed(2);
 
     // Show left analog stick and deadzone
-    leftDeadZone.style.width = controllerSettings.Deadzone + "%";
-    leftDeadZone.style.height = controllerSettings.Deadzone + "%";
+    leftDeadZone.style.width = leftDeadZone.style.height = `${controllerSettings.Deadzone}%`;
+    // leftDeadZone.style.height = `${controllerSettings.Deadzone}%`;
     leftVis.style.left = 50 + (gp.axes[controllerSettings.L_stick[0]] * 100) / 2 + "%";
     leftVis.style.top = 50 + (gp.axes[controllerSettings.L_stick[2]] * 100) / 2 + "%";
 
     // Buffer "A" button push
     if (gp.buttons[controllerSettings.A].pressed == true) {
         buffer.push({ direction: 0, time: timeStamp });
-        if (released == true && checkThreeMotionInput(buffer.slice().reverse(), inputsOrder.hadoken_L)) {
-            console.log("GOOD INPUT");
-            audioPlayer.src = sounds.ken_shoryuken;
-            audioPlayer.currentTime = 0;
-            audioPlayer.play();
-            // gp.vibrationActuator.playEffect("dual-rumble", {
-            //     startDelay: 0,
-            //     duration: 100,
-            //     weakMagnitude: 0.2,
-            //     strongMagnitude: 0.2,
-            // });
+        if (released == true) {
+            if (checkThreeMotionInput(buffer.slice().reverse(), inputsOrder[chosenInput])) {
+                console.log("GOOD INPUT");
+                audioPlayer.src = sounds[chosenInput];
+                audioPlayer.currentTime = 0;
+                audioPlayer.play();
+                // gp.vibrationActuator.playEffect("dual-rumble", {
+                //     startDelay: 0,
+                //     duration: 100,
+                //     weakMagnitude: 0.2,
+                //     strongMagnitude: 0.2,
+                // });
+                document.body.style.backgroundColor = "#001100";
+            } else {
+                document.body.style.backgroundColor = "#110000";
+            }
         }
 
         // Clear the buffer 
         buffer.fill({ direction: -1, time: window.performance.now() });
-        document.body.style.backgroundColor = "#222222";
         used = true;
         released = false;
     } else {
@@ -186,6 +224,7 @@ const update = (timeStamp) => {
     if (!used)
         buffer.push({ direction: -1, time: timeStamp });
 };
+
 
 // -- -- -- -- -- -- --    --     -- -- -- -- -- -- -- \\
 // -- -- -- -- -- -- -- MAIN LOOP -- -- -- -- -- -- -- \\
